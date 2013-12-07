401de4389c00d6495b6a8ed9f791dcac9c173211
Surya Kiran <suryakiranl@gmail.com>
2013-12-06 21:44:55 -0800

    Licensing information updated with confirmation from Jia

--- src/test/java/edu/cmu/sv/sdsp/api/APIIntegrationTest.java
+++ src/test/java/edu/cmu/sv/sdsp/api/APIIntegrationTest.java
@@ -3,9 +3,9 @@
  * All rights reserved. 
  * 
  * This program and the accompanying materials are made available
- * under the terms of dual licensing, one of which is the GNU 
- * Public License v2.0 which accompanies this distribution, and is 
- * available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * under the terms of dual licensing(GPL V2 for Research/Education
+ * purposes). GNU Public License v2.0 which accompanies this distribution
+ * is available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
--- src/test/java/edu/cmu/sv/sdsp/util/HttpHelper.java
+++ src/test/java/edu/cmu/sv/sdsp/util/HttpHelper.java
@@ -3,9 +3,9 @@
  * All rights reserved. 
  * 
  * This program and the accompanying materials are made available
- * under the terms of dual licensing, one of which is the GNU 
- * Public License v2.0 which accompanies this distribution, and is 
- * available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * under the terms of dual licensing(GPL V2 for Research/Education
+ * purposes). GNU Public License v2.0 which accompanies this distribution
+ * is available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
@@ -13,6 +13,7 @@
  * Please contact http://www.cmu.edu/silicon-valley/ for more specific
  * information.
  */
+
 package edu.cmu.sv.sdsp.util;
 
 import java.io.BufferedReader;
