d1a64938ad0e0dbc71ba9c39f32a23d4a8eabafd
Surya Kiran <suryakiranl@gmail.com>
2013-12-06 17:13:25 -0800

    Updating licensing header

--- src/main/java/edu/cmu/sv/App.java
+++ src/main/java/edu/cmu/sv/App.java
@@ -1,11 +1,17 @@
-/*
+/**
+ * Copyright (c) 2013 Carnegie Mellon University Silicon Valley. 
+ * All rights reserved. 
  * 
- Copyright (c) 2013 CMU-SV.
-All rights reserved. This program and the accompanying materials
-are made available under the terms of the GNU Public License v2.0
-which accompanies this distribution, and is available at
-http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- *
+ * This program and the accompanying materials are made available
+ * under the terms of dual licensing, one of which is the GNU 
+ * Public License v2.0 which accompanies this distribution, and is 
+ * available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * 
+ * Please contact http://www.cmu.edu/silicon-valley/ for more specific
+ * information.
  */
 package edu.cmu.sv;
 
